21e89b22a48827862a3ae4fef9b6bf7932c433ab
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-03-14 06:31:06 +0700

    add file structure

---
+++ db/index.js
@@ -0,0 +1,8 @@
+const { Pool } = require('pg');
+
+const config = {
+  dev: { database: 'project2' },
+  prod: { connectionString: process.env.DATABASE_URL },
+};
+
+module.exports = new Pool(process.env.DATABASE_URL ? config.prod : config.dev);
---
+++ middlewares/set_current_user.js
@@ -0,0 +1,22 @@
+const db = require('../db');
+
+function setCurrentUser(req, res, next) {
+  res.locals.currentUser = {};
+  const { userId } = req.session;
+
+  if (userId) {
+    const sql = `SELECT id, email FROM users WHERE id = ${userId}`;
+    db.query(sql, (err, dbRes) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.locals.currentUser = dbRes.rows[0];
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+}
+
+module.exports = setCurrentUser;
